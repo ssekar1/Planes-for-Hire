936662dd640e50ffa1139b86499b2d8d0df92f29
Anti-Alias <wcahill1@umbc.edu>
2015-03-13 13:03:41 -0400

    House Cleaning
    Did a little house cleaning on the login.php file.

--- www codes/login.php
+++ www codes/login.php
@@ -1,25 +1,33 @@
+<!-- Header code that brings in our consistant style -->
 <?php
-/*
-  comments goes here !!!
-*/
 	$debug = false;
 	include ("headHTML.html");
-	
-	print ("<label><strong><center><font size = \"6\" color = \"#676767\">Login</font></center></strong><br>");
-	print ("</label>");
-	
-	print ("<div id = \"login\">");
-	print ("<label>Enter your email ");
-			print ("<input name = \"name\" id = \"name\" type = \"text\" size = \"30\" maxlength = \"30\" class = \"input\"/><br><br>");
-		print ("</label>");
-	
-	print ("<label>Enter your password ");
-			print ("<input name = \"name\" id = \"name\" type = \"text\" size = \"30\" maxlength = \"30\" class = \"input\"/><br><br>");
-		print ("</label>");
-	
-	print ("<button type = \"button\" id = \"submitButton\" onclick= \"confirm()\">Submit</button><br><br>");
-	
-	print ("</div>");
+?>
 
+
+<!-- Page title -->
+<label><strong><center><font size = "6" color = "#676767">
+	Login
+</font></center></strong><br></label>
+
+
+<!-- Input fields -->
+<div id = "login">
+	<label>
+		Enter your email
+		<input name = "name" id = "name" type = "text" size = "30" maxlength = "30" class = "input"><br><br>
+	</label>
+	
+	<label>
+		Enter your password
+		<input name = "name" id = "name" type = "text" size = "30" maxlength = "30" class = "input"><br><br>
+	</label>
+	
+	<button type = "button" id = "submitButton" onclick= "confirm()">Submit</button><br><br>
+</div>
+
+
+<!-- Footer code that finishes stylizing the page -->
+<?php
 	include ("tailHTML.html");
 ?>
