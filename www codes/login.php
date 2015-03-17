bc7a5eef4819c3844815fa88f313efd7dabca02a
Tam Tran <tamtran1@umbc.edu>
2015-03-17 19:54:22 -0400

    login page now works
    must create at least one user in database to see effect in action. once
    you login you cannot log out. haha. must instead clear browser cache to
    destroy session.

--- www codes/login.php
+++ www codes/login.php
@@ -4,48 +4,32 @@
 */
 	$debug = false;
 	include ('link.php');
-	$LINK = new Link($debug);
+	$link = new Link($debug);
 	session_start();
 	
 	$error=''; // variable to hold error message
 	if (isset($_POST['submit']))
-	{
 		if (empty($_POST['username']) || empty($_POST['password']))
-		{
 			$error = "Username or Password is invalid";
-		} else
+		else
 		{
 			// Define $username and $password
-			$username=$_POST['username'];
-			$password=$_POST['password'];
+			$username = $_POST['username'];
+			$password = $_POST['password'];
 			
-			// Establishing Connection with Server by passing server_name, user_id and password as a parameter
-			$connection = mysql_connect("localhost", "root", "");
-			// To protect MySQL injection for Security purpose
-			$username = stripslashes($username);
-			$password = stripslashes($password);
-			$username = mysql_real_escape_string($username);
-			$password = mysql_real_escape_string($password);
-			// Selecting Database
-			$db = mysql_select_db("company", $connection);
-			// SQL query to fetch information of registerd users and finds user match.
-			$query = mysql_query("select * from login where password='$password' AND username='$username'", $connection);
+			$query = $link->executeQuery("select * from `customer_profile` where `password` = '".$password."' AND email = '".$username."'", $_SERVER["SCRIPT_NAME"]);
 			$rows = mysql_num_rows($query);
 			
 			if ($rows == 1)
-			{
-				$_SESSION['login_user']=$username; // Initializing Session
-				header("location: profile.php"); // Redirecting To Other Page
-			} else
-			{
+				$_SESSION['login_user'] = $username; // Initializing Session
+			else
 				$error = "Username or Password is invalid";
-			}
-			mysql_close($connection); // Closing Connection
+			
+			//mysql_close($connection); // Closing connection
 		}
-	}
 
-	if(isset($_SESSION['login_user']))
-		print ("<META http-equiv = \"REFRESH\" content = \"0; main.php\">");;
+	if(isset($_SESSION['login_user'])) //redirect to main page if login successful
+		header('Location: main.php');
 	
 	include ("headHTML.html");
 	
