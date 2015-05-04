e69809084aaad40a2055c0f00d555056b7178109
Tam Tran <tamtran1@umbc.edu>
2015-05-04 19:52:15 -0400

    removed backup codes

--- www codes_bak/Code Example/User Profile Page/User-Profile-master/components/authentication-check.php
+++
@@ -1,4 +0,0 @@
-<?php
-    header( "refresh:1;url=../login.php" ); 
-    echo "<center><h2>Wrong Username or Password (Your email id is you username). Redirecting shortly!</h2></center>";
-?>
--- www codes_bak/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start ();
-	if (session_destroy ()) // Destroying All Sessions
-		header ("Location: Main.php"); // Redirecting To Home Page
-	else
-		print ("<span>Error: unable to destroy session. Please clear browser's cache and cookies manually.</span>");
-?>
